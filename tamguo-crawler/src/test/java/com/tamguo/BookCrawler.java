193b3d965ca5384585e0ad457fe6a9dccdc3207b
chenfeida <chenfeida@sungoin.com>
2018-07-24 14:55:10 +0800

    测试提交

--- tamguo-crawler/src/test/java/com/tamguo/BookCrawler.java
+++ tamguo-crawler/src/test/java/com/tamguo/BookCrawler.java
@@ -1,23 +1,23 @@
 package com.tamguo;
 
-import com.tamguo.service.IBookService;
-import com.tamguo.service.ISubjectService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.tamguo.service.IBookService;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookCrawler {
 
-	@Autowired
+    @Autowired
     IBookService bookService;
-	
+
     @Test
     public void crawlerBook() throws Exception {
-        bookService.crawlerBook();
+        this.bookService.crawlerBook();
     }
-	
+
 }
