ba31fa293d3575e3d782041ba32364bda946c500
tamguo <tamguo@Lenovo>
2018-08-02 15:39:06 +0800

    优化列表

--- tamguo-oms/src/main/java/com/tamguo/modules/sys/model/condition/SysMenuCondition.java
+++ tamguo-oms/src/main/java/com/tamguo/modules/sys/model/condition/SysMenuCondition.java
@@ -1,6 +1,8 @@
 package com.tamguo.modules.sys.model.condition;
 
 public class SysMenuCondition {
+	
+	private String parentCode;
 
 	private Integer pageNo;
 	private Integer pageSize;
@@ -16,6 +18,12 @@ public class SysMenuCondition {
 	}
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
+	}
+	public String getParentCode() {
+		return parentCode;
+	}
+	public void setParentCode(String parentCode) {
+		this.parentCode = parentCode;
 	}
 	
 }
