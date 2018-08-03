4b96e82736cbacfa4ecdc23182b6c1194498c57d
tamguo <tamguo@Lenovo>
2018-08-03 19:20:29 +0800

    book

--- tamguo-oms/src/main/java/com/tamguo/modules/tiku/web/BookController.java
+++ tamguo-oms/src/main/java/com/tamguo/modules/tiku/web/BookController.java
@@ -1,10 +1,8 @@
 package com.tamguo.modules.tiku.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import com.tamguo.modules.tiku.service.IBookService;
 
 @Controller
 @RequestMapping(path="tiku/book")
@@ -12,9 +10,6 @@ public class BookController {
 
 	/** 书籍*/
 	private final String BOOK_LIST_PAGE = "modules/tiku/book/list";
-	
-	@Autowired
-	private IBookService iBookService;
 	
 	@RequestMapping(path="list")
 	public ModelAndView index(ModelAndView model) {
