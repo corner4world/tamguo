b51f833474e9f2c0b375953de2da37fd6b0050db
tamguo <tamguo@Lenovo>
2018-08-04 21:46:11 +0800

    书籍

---
+++ tamguo-tms/src/main/java/com/tamguo/web/tiku/BookController.java
@@ -0,0 +1,57 @@
+package com.tamguo.web.tiku;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.baomidou.mybatisplus.mapper.Condition;
+import com.tamguo.modules.tiku.model.BookEntity;
+import com.tamguo.modules.tiku.model.ChapterEntity;
+import com.tamguo.modules.tiku.model.CourseEntity;
+import com.tamguo.modules.tiku.model.SubjectEntity;
+import com.tamguo.modules.tiku.service.IBookService;
+import com.tamguo.modules.tiku.service.IChapterService;
+import com.tamguo.modules.tiku.service.ICourseService;
+import com.tamguo.modules.tiku.service.ISubjectService;
+
+@Controller
+public class BookController {
+
+	@Autowired
+	IBookService iBookService;
+	@Autowired
+	IChapterService iChapterService;
+	@Autowired
+	ISubjectService iSubjectService;
+	@Autowired
+	ICourseService iCourseService;
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = {"book/{uid}.html"}, method = RequestMethod.GET)
+	public ModelAndView index(@PathVariable String uid , ModelAndView model) {
+		try {
+			BookEntity book = iBookService.selectById(uid);
+			SubjectEntity subject = iSubjectService.selectById(book.getSubjectId());
+			List<CourseEntity> courseList = iCourseService.selectList(Condition.create().eq("subject_id", subject.getId()));
+			List<BookEntity> bookList = iBookService.selectList(Condition.create().eq("course_id", book.getCourseId()));
+			CourseEntity course = iCourseService.selectById(book.getCourseId());
+			List<ChapterEntity> chapterList = iChapterService.selectList(Condition.create().eq("book_id", uid));
+			model.addObject("book", book);
+			model.addObject("subject", subject);
+			model.addObject("course", course);
+			model.addObject("chapterList" , chapterList);
+			model.addObject("courseList", courseList);
+			model.addObject("bookList", bookList);
+			model.setViewName("book");
+			return model;
+		} catch (Exception e) {
+			model.setViewName("404");
+			return model;
+		}
+	}
+	
+}
