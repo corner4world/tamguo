b183266074b3b9af7ac62dc0ad773f7f81487ea8
tamguo <tamguo@Lenovo>
2018-08-26 14:42:44 +0800

    fix bug

--- tamguo-crawler/src/main/java/com/tamguo/dao/CrawlerPaperMapper.java
+++ tamguo-crawler/src/main/java/com/tamguo/dao/CrawlerPaperMapper.java
@@ -1,8 +1,17 @@
 package com.tamguo.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.baomidou.mybatisplus.plugins.Page;
 import com.tamguo.config.dao.SuperMapper;
 import com.tamguo.model.CrawlerPaperEntity;
 
 public interface CrawlerPaperMapper extends SuperMapper<CrawlerPaperEntity>{
+
+	@Select("SELECT c.* FROM crawler_paper c WHERE c.paper_id IN (SELECT p.id FROM tiku_paper p WHERE p.area_id = #{areaId}) order by id,queindex asc;")
+	List<CrawlerPaperEntity> selectPageByAreaId(Page<CrawlerPaperEntity> questionPage,@Param(value="areaId") String areaId);
 
 }
