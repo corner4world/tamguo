a563265f7cb6217ef2d50acbeaf292b0ee1505ac
tamguo <tamguo@Lenovo>
2018-10-10 10:56:57 +0800

    fix bug

--- tamguo-modules-core/src/main/java/com/tamguo/modules/member/service/IMemberService.java
+++ tamguo-modules-core/src/main/java/com/tamguo/modules/member/service/IMemberService.java
@@ -5,40 +5,52 @@ import com.tamguo.modules.member.model.MemberEntity;
 
 public interface IMemberService {
 
+	/** 登录*/
 	public Result login(String username , String password);
 	
+	/** 检查用户名*/
 	public Result checkUsername(String username);
 
+	/** 检查手机号*/
 	public Result checkMobile(String mobile);
 	
+	/** 注册*/
 	public Result register(MemberEntity member);
 
+	/** 检查账号*/
 	public Result checkAccount(String account);
 	
+	/** 确认账号*/
 	public Result confirmAccount(String account , String veritycode);
 
+	/** 安全检查*/
 	public Result securityCheck(String username , String isEmail , String vcode);
 
+	/** 重置密码*/
 	public Result resetPassword(String resetPasswordKey , String username , String password, String verifypwd);
 	
+	/** 获得登录错误次数*/
 	public Integer getLoginFailureCount(MemberEntity member);
 	
+	/** 修改会员*/
 	public void updateMember(MemberEntity member);
 	
+	/** 获取会员信息*/
 	public MemberEntity findByUid(String uid);
 	
+	/** 获取用户*/
 	public MemberEntity findByUsername(String username);
 	
+	/** 更新错误次数*/
 	public void updateLoginFailureCount(MemberEntity member , Integer loginFailureCount);
 
+	/** 更新最后登录时间*/
 	public void updateLastLoginTime(String string);
 
+	/** 获取当前用户*/
 	public MemberEntity findCurrMember(String id);
 
-	/**
-	 * 修改密码
-	 * @param member
-	 * @return
-	 */
+	/** 修改密码 */
 	public Result updatePwd(MemberEntity member);
+	
 }
