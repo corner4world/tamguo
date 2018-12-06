f7191c7c19fa1b6cbc5db6dc24e6e2535a875c35
tamguo <tamguo@Lenovo>
2018-12-06 10:52:46 +0800

    fix bug

--- tamguo-bms/src/main/java/com/tamguo/web/BookCategoryController.java
+++ tamguo-bms/src/main/java/com/tamguo/web/BookCategoryController.java
@@ -2,8 +2,8 @@ package com.tamguo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.baomidou.mybatisplus.mapper.Condition;
@@ -14,14 +14,8 @@ import com.tamguo.modules.book.service.IBookService;
 import com.tamguo.modules.member.model.MemberEntity;
 import com.tamguo.modules.member.service.IMemberService;
 
-/**
- * Controller - 首页
- * 
- * @author tamguo
- *
- */
 @Controller
-public class IndexController {
+public class BookCategoryController {
 	
 	@Autowired
 	IBookService iBookService;
@@ -29,14 +23,14 @@ public class IndexController {
 	IMemberService iMemberService;
 	@Autowired
 	IBookCategoryService iBookCategoryService;
-
+	
 	@SuppressWarnings("unchecked")
-	@RequestMapping(path= {"index" , "/" , "index.html"})
-	public ModelAndView index(Integer current , ModelAndView model) {
+	@RequestMapping(value="category/{id}.html")
+	public ModelAndView bookCategory(@PathVariable String id , Integer current , ModelAndView model) {
 		if(current == null) {
 			current = 1;
 		}
-		model.setViewName("index");
+		model.setViewName("category");
 		Page<BookEntity> bookPage = iBookService.selectPage(new Page<>(current, 18));
 		for(BookEntity book : bookPage.getRecords()) {
 			MemberEntity member = iMemberService.selectById(book.getOwner());
@@ -44,19 +38,11 @@ public class IndexController {
 				book.setMemberName(member.getUsername());
 			}
 		}
-		model.addObject("bookPage", bookPage);
-		
 		// 查询分类
 		model.addObject("bookCategoryList", iBookCategoryService.selectList(Condition.create().eq("parent_id", "0")));
-		model.setViewName("index");
-
+		model.addObject("categoryId", id);
+		model.addObject("bookPage", bookPage);
 		return model;
 	}
-	
-    @RequestMapping(value = "/baidu_verify_buLyB5tU19.html", method = RequestMethod.GET)
-    public ModelAndView baidu_verify_5agfTbCO3Q(ModelAndView model) {
-    	model.setViewName("thirdparty/baidu_verify_buLyB5tU19");
-        return model;
-    }
 	
 }
